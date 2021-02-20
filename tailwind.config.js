786ee8b2161099c539dde8643f6c6ad7bf2bd0d0
ScottPenhall98 <scottpenhall98@gmail.com>
2021-02-20 21:33:21 +1300

    got an image placed in the home page of a wheel that spins from tailwind key frames and animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,19 +6,29 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors:{
+    colors: {
       burple: '6100ff',
       white: '#fff',
       gray: {
         leftNav: '#343434',
         darkTheme: '#272727',
-        active: '#5a5a5a'
-      }
+        active: '#5a5a5a',
+      },
     },
-    extend: {},
+    extend: {
+      animation: {
+        'spin-slow': 'spin-anti-clockwise 0.5s linear infinite',
+      },
+      keyframes: {
+        'spin-anti-clockwise': {
+          '0%' : {transform: 'rotate(360deg)'},
+          '100%': {transform: 'rotate(0deg)'},
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
